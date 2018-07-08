0b51b2fd9b547eb210116a3cc4898107dcb522f0
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-07-08 19:46:11 +0100

    refactor: refactor the element serialization functionality, create validity test methods according t

--- src/modules/Serializer.js
+++ src/modules/Serializer.js
@@ -18,6 +18,95 @@ export default class Serializser {
     }
 
     /**
+     * tests if the given text value is a valid xml name production
+     *@param {string} value - the text value
+     *@returns {boolean}
+    */
+    validateXMLNameProduction(value) {
+        let nameStart = '[a-z]|[:]|[_]|[\\u00C0-\\u00D6]|[\\u00D8-\\u00F6]|[\\u00F8-\\u02FF]|' +
+        '[\\u0370-\\u037D]|[\\u037F-\\u1FFF]|[\\u200C-\\u200D]|[\\u2070-\\u218F]|' +
+        '[\\u2C00-\\u2FEF]|[\\u3001-\\uD7FF]|[\\uF900-\\uFDCF]|[\\uFDF0-\\uFFFD]|' +
+        '[\\uD800-\\uDBFF]|[\\uDC00-\\uDFFF]',
+
+        nameChar = nameStart + '|[\\-]|[.]|[0-9]|\\u00B7|[\\u0300-\\u036F]|[\\u203F-\\u2040]';
+
+        let regex = new RegExp(`^(${nameStart})(${nameChar})*`, 'i');
+        return typeof value === 'string' && regex.test(value);
+    }
+
+    /**
+     * tests if the given text value is a valid xml tag name
+     *@param {string} value - the text value
+     *@returns {boolean}
+    */
+    validateXMLTagName(value) {
+        return this.validateXMLNameProduction(value) && !/^xml/i.test(value);
+    }
+
+    /**
+     * tests if the given text value is a valid xml attribute name
+     *@param {string} value - the text value
+     *@param {string} attrNS - attribute namespace value
+     *@returns {boolean}
+    */
+    validateXMLAttrName(value, attrNS) {
+        return this.validateXMLNameProduction(value) &&
+            (value.toLowerCase() !== 'xmlns' || attrNS !== null);
+    }
+
+    /**
+     * validates xml char
+     *@param {string} value - char to validate
+     *@returns {boolean}
+    */
+    validateChar(value) {
+        let char = '[\\u0009]|[\\u000A]|[\\u000D]|[\\u0020-\uD7FF]|[\\uE000-\\uFFFD]|' +
+        '[\\uD800-\\uDBFF]|[\\uDC00-\\uDFFF]';
+
+        let regex = new RegExp(`^(${char})*$`);
+        return typeof value === 'string' && regex.test(value);
+    }
+
+    /**
+     * checks if the given tuple consisting of namespaceURI and localName pair exists in the records
+     *@param {Array} records - tuple records
+     *@param {Array} tuple - the tuple to check
+     *@returns {boolean}
+    */
+    tupleExists(records, tuple) {
+        let exists = false, len = tuple.length;
+        for (let record of records) {
+            if (len === record.length) {
+                exists = true;
+                let i = -1;
+                while (++i < len) {
+                    if (record[i] !== tuple[i]) {
+                        exists = false;
+                        break;
+                    }
+                }
+            }
+            if (exists)
+                break;
+        }
+        return exists;
+    }
+
+    /**
+     * generate a element prefix
+     *@param {Map} map - namespace prefix map,
+     *@param {string} ns - the new namespace
+     *@returns {string}
+     *@see https://www.w3.org/TR/DOM-Parsing/#dfn-concept-generate-prefix
+    */
+    generatePrefix(map, ns) {
+        let generatedPrefix = 'ns' + this.prefixIndex++;
+        map[ns] = generatedPrefix;
+
+        return generatedPrefix;
+    }
+
+    /**
      * records the namespace information for an element
      *@param {Element} elem - the element node
      *@param {Map} prefixMap - element prefix to namespace map
@@ -62,6 +151,103 @@ export default class Serializser {
     }
 
     /**
+     * serializes an attribute value given an attribute value and require well-formed flag
+     *@param {string} value - the attribute value
+     *@param {boolean} requireWellFormed - boolean value indicating if well formedness is a
+     * requirement
+     *@returns {string}
+     *@see https://www.w3.org/TR/DOM-Parsing/#dfn-concept-serialize-attr-value
+    */
+    serializeAttrValue(value, requireWellFormed) {
+        if (requireWellFormed && !this.validateChar(value))
+            throw new Error(value + ' is not a valid attribute value');
+
+        if (value === null)
+            return '';
+
+        return value.replace('"', '&quot;').replace('&', '&amp;').replace('<', '&lt;')
+            .replace('>', '&gt;');
+    }
+
+    /**
+     * produces the XML serialization of the attributes of an element
+     *@param {Element} node - the element node
+     *@param {Map} map - namespace prefix map,
+     *@param {boolean} ignoreNSDefAttr - a boolean ignore namespace definition attribute flag,
+     *@param {string} dupPrefixDef - a duplicate prefix definition value
+     *@param {boolean} requireWellFormed - a boolean require well-formed xml flag
+     *@returns {string}
+     *@see https://www.w3.org/TR/DOM-Parsing/#dfn-concept-serialize-xml-attributes
+    */
+    serializeAttributes(node, map, ignoreNSDefAttr, requireWellFormed) {
+        //STEP 1, 2
+        let result = '', localNameSet = [];
+
+        let attributes = node.attributes, i = -1,
+        len = attributes.length;
+
+        //STEP 3
+        while (++i < len) {
+            let attr = attributes[i], nsURI = attr.namespaceURI, localName = attr.localName,
+            prefix = attr.prefix;
+
+            let tuple = [nsURI, localName];
+            //STEP 3.1
+            /* istanbul ignore if */
+            if (requireWellFormed && this.tupleExists(localNameSet, tuple))
+                throw new Error('element cannot have two attributes with the same namespaceURI and localName');
+
+            //STEP 3.2
+            localNameSet.push(tuple);
+
+            //STEP 3. 3, 4
+            let attrNS = nsURI, candidatePrefix = null;
+            //STEP 3.5
+            /* istanbul ignore else */
+            if (attrNS !== null) {
+                //STEP 3.5.1
+                if (attrNS === XMLNS_NS && ((prefix === null && ignoreNSDefAttr) ||
+                    (prefix !== null && this.dupPrefixDef.includes(localName)))) {
+                    continue;
+                }
+
+                else if (prefix === null) {
+                    candidatePrefix = null;
+                }
+
+                //STEP 3.5.2
+                else if (typeof map[attrNS] !== 'undefined') {
+                    candidatePrefix = map[attrNS];
+                }
+
+                //STEP 3.5.3
+                else {
+                    candidatePrefix = this.generatePrefix(map, attrNS);
+                    result += ` xmlns:${candidatePrefix}="${this.serializeAttrValue(attrNS)}"`;
+                }
+            }
+
+            //STEP 3.6
+            result += ' ';
+
+            //STEP 3.7
+            if(candidatePrefix !== null)
+                result += candidatePrefix + ':';
+
+            //STEP 3.8
+            /* istanbul ignore if */
+            if (requireWellFormed && !this.validateXMLAttrName(localName, attrNS))
+                throw new Error(localName + ' is not a valid xml attribute name');
+
+            //STEP 3.9
+            result += `${localName}="${this.serializeAttrValue(attr.value)}"`;
+        }
+
+        //STEP 4
+        return result;
+    }
+
+    /**
      * runs the XML serialization algorithm on an element node
      *@param {Element} node - the element node.
      *@param {string} namespace - context namespace
@@ -96,6 +282,131 @@ export default class Serializser {
         //delcare inherited namespace as parent namespace, and ns as node namespace uri
         inheritedNS = namespace,
         ns = node.namespaceURI;
+
+        //STEP 12
+        if(inheritedNS === ns) {
+            //STEP 12.1
+            /* istanbul ignore if */
+            if (localDefNS !== null)
+                ignoreNSDefAttr = true;
+
+            //STEP 12.2
+            /* istanbul ignore if */
+            if (ns === XML_NS)
+                qualifiedName = 'xml:' + localName;
+
+            //STEP 12.3
+            else
+                qualifiedName = localName;
+
+            //STEP 12.4
+            markup += qualifiedName;
+        }
+
+        //STEP 13
+        else {
+            //STEP 13. 1, 2
+            let prefix = node.prefix,
+            candidatePrefix = typeof map[ns] !== 'undefined'? map[ns] : null;
+
+            //STEP 13.3
+            /* istanbul ignore else */
+            if (candidatePrefix !== null) {
+                //STEP 13.3.1
+                qualifiedName = candidatePrefix + ':' + localName;
+
+                //STEP 13.3.2
+                /* istanbul ignore if */
+                if (localDefNS !== null)
+                    inheritedNS = ns;
+
+                //STEP 13.3.3
+                markup += qualifiedName;
+            }
+
+            //STEP 13.4
+            else if (prefix !== null && localDefNS === null) {
+                //STEP 13.4.1
+                if(elmPrefixList.includes(prefix)) {
+                    // it will use the prefixIndex instance variable. as js can't pass values by reference
+                    prefix = this.generatePrefix(map, ns);
+                }
+                //STEP 13.4.2
+                else {
+                    map[ns] = prefix;
+                }
+
+                //STEP 13.4.3
+                qualifiedName = prefix + ':' + localName;
+                //STEP 13.4.4
+                markup += qualifiedName;
+
+                //STEP 13.4.5. 1, 2, 3, 4, 5, 6
+                markup += ` xmlns:${prefix}="${this.serializeAttrValue(ns, requireWellFormed)}"`;
+            }
+
+            //STEP 13.5
+            else if (localDefNS === null || localDefNS !== ns) {
+                //STEP 13.5. 1, 2, 3
+                ignoreNSDefAttr = true;
+                qualifiedName = localName;
+                inheritedNS = ns;
+
+                //STEP 13.5.4
+                markup += qualifiedName;
+
+                //STEP 13.5.5. 1, 2, 3, 4, 5, 6
+                markup += ` xmlns="${this.serializeAttrValue(ns, requireWellFormed)}"`;
+            }
+
+            //STEP 13.6
+            else {
+                qualifiedName = localName;
+                inheritedNS = ns;
+                markup += qualifiedName;
+            }
+        }
+
+        //STEP 14
+        markup += this.serializeAttributes(node, map, ignoreNSDefAttr, requireWellFormed);
+
+        //STEP 15
+        if (ns === HTML_NS && !node.hasChildNodes() &&
+            /^(area|base|basefont|bgsound|br|col|embed|frame|hr|img|input|keygen|link|menuitem|meta|param|source|track|wbr)$/i.test(localName)) {
+            markup += ' /';
+            skipEndTag = true;
+        }
+
+        //STEP 16
+        if (ns !== HTML_NS && !node.hasChildNodes()) {
+            markup += '/';
+            skipEndTag = true;
+        }
+
+        //STEP 17
+        markup += '>';
+
+        //STEP 18
+        if (skipEndTag)
+            return markup;
+
+        //STEP 19
+        if (ns === HTML_NS && localName.toLowerCase() === 'template') {
+            markup += this.runSerialization(node.content, inheritedNS, map, requireWellFormed);
+        }
+
+        //STEP 20
+        else {
+            let len = node.childNodes.length, i = -1;
+            while (++i < len) {
+                markup += this.runSerialization(node.childNodes[i], inheritedNS, map, requireWellFormed);
+            }
+        }
+
+        //STEP 21
+        markup += '</' + qualifiedName + '>';
+
+        return markup;
     }
 
     /**
